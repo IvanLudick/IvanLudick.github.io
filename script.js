69b0a9f93faa444c2d74b0911c89f7551075513c
IvanLudick <smiley20089@gmail.com>
2022-12-20 16:25:33 +0200

    scroll disabled if menu opened, close menu btn

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('item-nav')[0];
 const bodyColor = document.getElementsByClassName('section-1')[0];
 const toggleButtonOff = document.getElementsByClassName('toggle-2')[0];
+const toggleScroll = document.getElementsByTagName('body')[0];
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')(bodyColor.classList.toggle('change'));
+  navbarLinks.classList.toggle('active')(bodyColor.classList.toggle('change')(toggleScroll.classList.toggle('fixed')));
 });
 
 toggleButtonOff.addEventListener('click', () => {
-  if (navbarLinks.classList.toggle('active')(bodyColor.classList.toggle('change'))) {
+  if (navbarLinks.classList.toggle('active')(bodyColor.classList.toggle('change')(toggleScroll.classList.toggle('fixed')))) {
     toggleButtonOff();
   }
 });
