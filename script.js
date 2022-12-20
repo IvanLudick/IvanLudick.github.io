c48886f0eb18ed2541a263ee626fef2ce976ae16
IvanLudick <smiley20089@gmail.com>
2022-12-20 08:36:40 +0200

    mobile menu button update and blur effect

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('item-nav')[0]
-const bodyColor = document.getElementsByTagName('body')[0]
+const bodyColor = document.getElementsByClassName('section-1')[0]
+const toggleButtonOff = document.getElementsByClassName('toggle-2')[0]
 
 toggleButton.addEventListener('click', () => {
   if (navbarLinks.classList.toggle('active'),
@@ -8,5 +9,12 @@ toggleButton.addEventListener('click', () => {
     toggleButton();
     color();
   }
-  
+})
+
+toggleButtonOff.addEventListener('click', () => {
+  if (navbarLinks.classList.toggle('active'),
+    bodyColor.classList.toggle('change')) {
+    toggleButtonOff();
+    color();
+  }
 })
