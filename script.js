8793cca0526560499e3d591e79db96387de7df14
IvanLudick <smiley20089@gmail.com>
2022-12-20 09:58:43 +0200

    javascript linters added

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('item-nav')[0]
-const bodyColor = document.getElementsByClassName('section-1')[0]
-const toggleButtonOff = document.getElementsByClassName('toggle-2')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('item-nav')[0];
+const bodyColor = document.getElementsByClassName('section-1')[0];
+const toggleButtonOff = document.getElementsByClassName('toggle-2')[0];
 
 toggleButton.addEventListener('click', () => {
-  if (navbarLinks.classList.toggle('active'),
-    bodyColor.classList.toggle('change')) {
+  if (navbarLinks.classList.toggle('active')(bodyColor.classList.toggle('change'))) {
     toggleButton();
-    color();
   }
-})
+});
 
 toggleButtonOff.addEventListener('click', () => {
-  if (navbarLinks.classList.toggle('active'),
-    bodyColor.classList.toggle('change')) {
+  if (navbarLinks.classList.toggle('active')(bodyColor.classList.toggle('change'))) {
     toggleButtonOff();
-    color();
   }
-})
+});
