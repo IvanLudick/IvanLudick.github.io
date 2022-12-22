eabf8b8245ea8e42cd8bce2a52a4e4ff80673ccc
IvanLudick <smiley20089@gmail.com>
2022-12-22 09:39:55 +0200

    mobile and desktop popup added

--- script.js
+++ script.js
@@ -18,3 +18,61 @@ toggleButtonOff.addEventListener('click', () => {
 function closeMenu(toggleButton) {
   navbarLinks.classList.toggle('active')(bodyColor.classList.toggle('change')(toggleScroll.classList.toggle('fixed')));
 }
+
+const toggleTonicPop = document.getElementsByClassName('project-btn')[0];
+const tonicPopup = document.getElementsByClassName('tonic-popup')[0];
+const tonicPopClose = document.getElementsByClassName('toggle-popup')[0];
+const popupBlur = document.getElementsByClassName('header-section')[0];
+const blurNav = document.getElementsByClassName('toolbar')[0];
+
+toggleTonicPop.addEventListener('click', () => {
+  tonicPopup.classList.toggle('show')(popupBlur.classList.toggle('blur')(blurNav.classList.toggle('dim')));
+});
+
+tonicPopClose.addEventListener('click', () => {
+  if (tonicPopup.classList.toggle('show')(popupBlur.classList.toggle('blur')(blurNav.classList.toggle('dim')))) {
+    tonicPopClose();
+  }
+});
+
+const toggleMultiPop = document.getElementsByClassName('project-btn2')[0];
+const multiPopup = document.getElementsByClassName('multi-popup')[0];
+const multiPopClose = document.getElementsByClassName('toggle-popup2')[0];
+
+toggleMultiPop.addEventListener('click', () => {
+  multiPopup.classList.toggle('show')(popupBlur.classList.toggle('blur')(blurNav.classList.toggle('dim')));
+});
+
+multiPopClose.addEventListener('click', () => {
+  if (multiPopup.classList.toggle('show')(popupBlur.classList.toggle('blur')(blurNav.classList.toggle('dim')))) {
+    tonicPopClose();
+  }
+});
+
+const toggleFacePop = document.getElementsByClassName('project-btn3')[0];
+const facePopup = document.getElementsByClassName('facebook-popup')[0];
+const facePopClose = document.getElementsByClassName('toggle-popup3')[0];
+
+toggleFacePop.addEventListener('click', () => {
+  facePopup.classList.toggle('show')(popupBlur.classList.toggle('blur')(blurNav.classList.toggle('dim')));
+});
+
+facePopClose.addEventListener('click', () => {
+  if (facePopup.classList.toggle('show')(popupBlur.classList.toggle('blur')(blurNav.classList.toggle('dim')))) {
+    tonicPopClose();
+  }
+});
+
+const toggleUberPop = document.getElementsByClassName('project-btn4')[0];
+const uberPopup = document.getElementsByClassName('uber-popup')[0];
+const uberPopClose = document.getElementsByClassName('toggle-popup4')[0];
+
+toggleUberPop.addEventListener('click', () => {
+  uberPopup.classList.toggle('show')(popupBlur.classList.toggle('blur')(blurNav.classList.toggle('dim')));
+});
+
+uberPopClose.addEventListener('click', () => {
+  if (uberPopup.classList.toggle('show')(popupBlur.classList.toggle('blur')(blurNav.classList.toggle('dim')))) {
+    tonicPopClose();
+  }
+});
